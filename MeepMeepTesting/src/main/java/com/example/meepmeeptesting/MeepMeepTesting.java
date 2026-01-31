709d6e01951dec20b41b8ed322ec83b998591afe
Lincoln Cogdill <lincolncogdill@gmail.com>
2026-01-31 13:40:23 -0600

    All tested, looks great!

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -24,40 +24,43 @@ public class MeepMeepTesting {
 
         Pose2d startPose = new Pose2d(0, 0, Math.toRadians(90));
 
+        // Adjust if balls are flying too far left/right
+        int shootingRotationDeg = 140;
+
         Action initialMove = drive.actionBuilder(startPose)
                 .strafeToSplineHeading(new Vector2d(-53, 48), Math.toRadians(310))
-                .strafeToLinearHeading(new Vector2d(-20, 20), Math.toRadians(135))
+                .strafeToLinearHeading(new Vector2d(-20, 20), Math.toRadians(shootingRotationDeg))
                 .waitSeconds(1)
                 .build();
 
         int xAdjustmentNum = 24;
         int xAdjustMult = 0;
 
-        Action ballCollect1 = drive.actionBuilder(new Pose2d(-20, 20, Math.toRadians(135)))
+        Action ballCollect1 = drive.actionBuilder(new Pose2d(-20, 20, Math.toRadians(shootingRotationDeg)))
                 .strafeToLinearHeading(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 20), Math.toRadians(90))
                 .strafeTo(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 54.0))
                 .strafeTo(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 40))
-                .splineToSplineHeading(new Pose2d(-20, 20, Math.toRadians(140)), Math.toRadians(200))
+                .splineToSplineHeading(new Pose2d(-20, 20, Math.toRadians(shootingRotationDeg)), Math.toRadians(200))
                 .waitSeconds(1)
                 .build();
 
         xAdjustMult = 1;
 
-        Action ballCollect2 = drive.actionBuilder(new Pose2d(-20, 20, Math.toRadians(135)))
+        Action ballCollect2 = drive.actionBuilder(new Pose2d(-20, 20, Math.toRadians(shootingRotationDeg)))
                 .strafeToLinearHeading(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 20), Math.toRadians(90))
                 .strafeTo(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 54.0))
                 .strafeTo(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 40))
-                .splineToSplineHeading(new Pose2d(-20, 20, Math.toRadians(140)), Math.toRadians(200))
+                .splineToSplineHeading(new Pose2d(-20, 20, Math.toRadians(shootingRotationDeg)), Math.toRadians(200))
                 .waitSeconds(1)
                 .build();
 
         xAdjustMult = 2;
 
-        Action ballCollect3 = drive.actionBuilder(new Pose2d(-20, 20, Math.toRadians(135)))
+        Action ballCollect3 = drive.actionBuilder(new Pose2d(-20, 20, Math.toRadians(shootingRotationDeg)))
                 .strafeToLinearHeading(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 20), Math.toRadians(90))
                 .strafeTo(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 54.0))
                 .strafeTo(new Vector2d(-12.0 + xAdjustmentNum*xAdjustMult, 40))
-                .splineToSplineHeading(new Pose2d(-20, 20, Math.toRadians(140)), Math.toRadians(200))
+                .splineToSplineHeading(new Pose2d(-20, 20, Math.toRadians(shootingRotationDeg)), Math.toRadians(200))
                 .waitSeconds(1)
                 .build();
 
