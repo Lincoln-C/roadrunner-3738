626577a6763ca93778ea3f3843739a7c3dffa5ab
Lincoln Cogdill <lincolncogdill@gmail.com>
2026-06-21 16:18:30 -0500

    Disable OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldRobotCentricMechOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldRobotCentricMechOne.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,10 +11,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 
-@TeleOp
+@TeleOp @Disabled
 
 public class FieldRobotCentricMechOne extends LinearOpMode {
-    
     private DcMotor leftBack;
     private DcMotor rightBack;
     private DcMotor rightFront;
